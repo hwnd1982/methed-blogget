587dca9b45cca37906a668cf4b6e523a20d3f800
Kirill Lavrov <lkv@myttk.ru>
2022-08-02 15:23:17 +0700

    implemented search on saga

--- src/store/search/search.saga.js
+++ src/store/search/search.saga.js
@@ -1,40 +1,35 @@
 import axios from 'axios';
 import {put, select, takeLatest} from 'redux-saga/effects';
 import {URL_API} from '../../api/const';
-import {searchRequestSuccess, SEARCH_REQUEST} from './search';
+import {searchRequestError, searchRequestSuccess, SEARCH_REQUEST} from './search';
 
-const fetchSearch = async (token, search) => {
-  const response = await axios(`${URL_API}/search?q=${search}`, {
-    headers: {
-      Authorization: `bearer ${token}`,
-    },
-  });
+function* fetchSearch({search}) {
+  const token = yield select(store => store.token.token);
+  try {
+    const response = yield axios(`${URL_API}/search?q=${search}`, {
+      headers: {
+        Authorization: `bearer ${token}`,
+      },
+    });
 
-  const {data: {children: posts, after}} = response.data;
-  const data = posts.map(({data: {id, title, selftext, thumbnail, author, ups, created}}) =>
-    ({
-      id,
-      title,
-      selftext,
-      thumbnail: /^https:\/\//.test(thumbnail) ? thumbnail.replace(/\?.*$/, '') : '',
-      author,
-      ups,
-      date: created
-    }));
+    const {data: {children: posts, after}} = response.data;
+    const data = posts.map(({data: {id, title, selftext, thumbnail, author, ups, created}}) =>
+      ({
+        id,
+        title,
+        selftext,
+        thumbnail: /^https:\/\//.test(thumbnail) ? thumbnail.replace(/\?.*$/, '') : '',
+        author,
+        ups,
+        date: created
+      }));
 
-  return {data, after};
-};
-
-const getToken = state => state.token.token;
-
-function* workerSearch(action) {
-  const token = yield select(getToken);
-  const data = yield fetchSearch(token, action);
-  yield put(searchRequestSuccess(data));
-
-  console.log('data', data);
+    yield put(searchRequestSuccess({data, after}));
+  } catch (error) {
+    yield put(searchRequestError(error));
+  }
 }
 
 export function* watchSearch() {
-  yield takeLatest(SEARCH_REQUEST, workerSearch);
+  yield takeLatest(SEARCH_REQUEST, fetchSearch);
 }
