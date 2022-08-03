29872bfde352884d90f7cdcf3448365546c29d76
Kirill Lavrov <lkv@myttk.ru>
2022-08-03 18:39:22 +0700

    hosting address change

--- src/api/const.js
+++ src/api/const.js
@@ -3,5 +3,5 @@ export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
 export const CLIENT_ID = 'rTprKrR3YsTygC8ql897wA';
 export const RESPONSE_TYPE = 'token';
 export const RANDOM_STRING = 'random_string';
-export const REDIRECT_URI = 'http://localhost:3000/auth';
+export const REDIRECT_URI = 'https://methed-blogget-lac.vercel.app/auth';
 export const SCOPE = 'identity read submit';
